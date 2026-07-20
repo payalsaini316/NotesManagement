a5b2f36d80a818cc2d3b61dec16eb275128c0c98
payalsaini316 <payalsaini316@gmail.com>
2026-07-21 00:38:06 +0530

    Updated MongoDB Atlas connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/notesdb");
+        await mongoose.connect("mongodb+srv://fullstackdatabase:payal1234@cluster0.svzg2ws.mongodb.net/?appName=Cluster0");
 
         console.log("MongoDB Connected Successfully");
     } catch (error) {
